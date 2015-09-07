b0dd8be1f4de22040e0f9139d3fec772d74e53e6
BasilMason <basilmason@hotmail.com>
2015-09-07 21:24:18 +0100

    String and Bytewise methods added

--- src/CryptoMain.java
+++ src/CryptoMain.java
@@ -7,7 +7,8 @@ public class CryptoMain {
 
     private void launch() {
 
-        set1Challenge1();
+        //set1Challenge1();
+        set1Challenge2();
 
     }
 
@@ -25,6 +26,24 @@ public class CryptoMain {
         System.out.println("Base64 string:\t\t" + base64);
         System.out.println("Hex converted:\t\t" + CryptoUtils.hexStringToBase64(hex));
         System.out.println("Base64 converted:\t" + CryptoUtils.base64StringToHex(base64));
+
+    }
+
+    private void set1Challenge2() {
+
+        /**
+         * Challenge 2
+         * Fixed XOR
+         */
+
+        String hex1 = "1c0111001f010100061a024b53535009181c";
+        String hex2 = "686974207468652062756c6c277320657965";
+        String xor = "746865206b696420646f6e277420706c6179";
+
+        System.out.println("First string:\t\t" + hex1);
+        System.out.println("Second string:\t\t" + hex2);
+        System.out.println("XOR result:\t\t\t" + CryptoUtils.xorHexStrings(hex1, hex2));
+        System.out.println("Expected result:\t" + xor);
 
     }
 
--- src/CryptoUtils.java
+++ src/CryptoUtils.java
@@ -6,6 +6,12 @@ import java.util.Base64;
 
 public class CryptoUtils {
 
+    /* ************************************************** */
+    /* ********  B Y T E W I S E  M E T H O D S  ******** */
+    /* ************************************************** */
+    /* methods performed directly on bytes ************** */
+    /* ************************************************** */
+
     /**
      * converts hexidecimal byte to java primitive type byte
      * @param hexByte to be converted
@@ -13,6 +19,47 @@ public class CryptoUtils {
      */
     public static byte hexByteToByte(String hexByte) {
         return (byte) Integer.parseInt(hexByte, 16);
+    }
+
+    /**
+     * converts hexidecimal byte to String containing binary
+     * byte
+     * @param hexByte
+     * @return
+     */
+    public static String hexByteToBinary(String hexByte) {
+
+        int i = Integer.parseInt(hexByte, 16);
+        String bin = Integer.toBinaryString(i);
+
+        while(bin.length() < 8) {
+
+            bin = "0" + bin;
+
+        }
+
+        return bin;
+    }
+
+    /**
+     * Performs the bitwise XOR operation on two hexidecimal bytes
+     * @param hexByte1
+     * @param hexByte2
+     * @return
+     */
+    public static String xorHexBytes(String hexByte1, String hexByte2) {
+
+        StringBuilder xor = new StringBuilder();
+
+        for (int i = 0; i < hexByte1.length(); i++) {
+
+            int r = Character.getNumericValue(hexByte1.charAt(i)) ^ Character.getNumericValue(hexByte2.charAt(i));
+
+            xor.append(r);
+
+        }
+
+        return xor.toString();
     }
 
     /* ************************************************** */
@@ -41,6 +88,80 @@ public class CryptoUtils {
         return sb.toString();
     }
 
+
+    /**
+     *
+     * @param hex
+     * @return
+     */
+    public static String hexStringToBinary(String hex) {
+
+        StringBuilder bin = new StringBuilder();
+        String byte1;
+
+        for (int i = 0; i < hex.length() / 2; i++) {
+
+            byte1 = hex.substring(2*i, 2*i + 2);
+
+            bin.append(hexByteToBinary(byte1));
+
+        }
+
+        return bin.toString();
+
+    }
+
+    /**
+     *
+     * @param bin
+     * @return
+     */
+    public static String binaryStringToHex(String bin) {
+
+        StringBuilder hex = new StringBuilder();
+
+        for (int i = 0; i < bin.length() / 8; i++) {
+
+            String s = Integer.toHexString(Integer.parseInt(bin.substring(8*i, 8*i + 8), 2));
+
+            if (s.length() < 2)
+                s = "0" + s;
+
+            hex.append(s);
+
+        }
+
+        return hex.toString();
+
+    }
+
+    /**
+     *
+     * @param hex1
+     * @param hex2
+     * @return
+     */
+    public static String xorHexStrings(String hex1, String hex2) {
+
+        // string declarations
+
+        String bin1, bin2, binXor;
+
+        // convert hex strings to binary strings
+
+        bin1 = hexStringToBinary(hex1);
+        bin2 = hexStringToBinary(hex2);
+
+        // perform xor operations
+
+        binXor = xorHexBytes(bin1, bin2);
+
+        // convert binary result to hex string
+
+        return binaryStringToHex(binXor);
+
+    }
+
     /**
      *
      * @param hex
@@ -49,9 +170,9 @@ public class CryptoUtils {
     public static String hexStringToBase64(String hex) {
         byte[] data = new byte[hex.length() / 2];
         for (int i = 0; i < hex.length() / 2; i++) {
-            data[i] = hexByteToByte(hex.substring(2*i, 2*i + 2));
+            data[i] = hexByteToByte(hex.substring(2 * i, 2 * i + 2));
         }
-        return Base64.getEncoder().encode(data).toString();
-    }
 
+        return new String(Base64.getEncoder().encode(data));
+    }
 }
